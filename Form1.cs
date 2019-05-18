bb273cc9b85eab60c5b55d057646f15f5b937a82
Mishko <44136629+MishkaSorochinskiy@users.noreply.github.com>
2019-05-18 11:20:26 +0300

    Added files

---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace PiersonProject
+{
+    public partial class TableGrid : Form
+    {
+        public static Excel.Application MyApp = null;
+
+        public TableGrid()
+        {
+            InitializeComponent();
+
+            TableGrid.MyApp = new Excel.Application();
+
+            this.openFileDialog1.FileName = @"C:\Users\misha\University\C#\PiersonProject\PiersonProject\DataTable.txt";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.startProgress();
+
+            List<item> pair_of_values = null;
+
+            try
+            {
+                if (this.checkRead.Checked)
+                {
+                    pair_of_values = ExtensionClass.generateFromFile(this.openFileDialog1.FileName);
+                }
+                else
+                {
+                    pair_of_values = ExtensionClass.generateNumbers();
+                }
+
+                item.N = pair_of_values.Sum(delegate (item val) { return val.Ni; });
+                item.lamb = pair_of_values.Sum(delegate (item val) { return val.XiNi; }) / item.N;
+
+                ExtensionClass.mergeCells(pair_of_values);
+
+                if (pair_of_values.Count <= 2)
+                {
+                    throw new Exception();
+                }
+
+                this.ShowResult(pair_of_values);
+
+                this.SumP.Text = pair_of_values.Sum(delegate (item val) { return val.Pi; }).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Smth went wrong!!! Problem can be detected in given file..", "PiersonProjectError", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+          
+      
+        }
+
+        public  void startProgress()
+        {
+          this.progress.Value = 0;
+
+          for (int i = 0; i < 4; ++i)
+          {
+            this.progress.Value += 5;
+          }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.labelX1.Text = "value";
+            this.labelX2.Text = "value";
+            this.tableData.DataSource = null;
+            this.progress.Value = 0;
+        }
+
+        private void ShowResult(List<item> pair_of_values)
+        {
+            this.tableData.DataSource = pair_of_values;
+
+            double X1 = pair_of_values.Sum(delegate (item val) { return val.value; });
+            double X2 = Math.Round(MyApp.WorksheetFunction.ChiInv(double.Parse((this.alphaval.Text)), pair_of_values.Count - 2), 5);
+
+            this.labelX1.Text = X1.ToString();
+            this.labelX2.Text = X2.ToString();
+
+            if (X1 < X2)
+            {
+                this.answ.Text = "Hypothesis is accepted!!!!!";
+            }
+            else
+            {
+                this.answ.Text = "Hypothesis is NOT accepted!!!!!";
+            }
+        }
+
+        private void searchFile_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+        }
+    }
+}
---
+++ item.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace PiersonProject
+{
+    class item
+    {
+        static public double lamb=0;
+        static public double N = 0;
+
+        public int Xi { get; set; }
+
+        public int Ni { get; set; }
+
+        public double XiNi
+        {
+            get
+            {
+                return this.Xi * this.Ni;
+            }
+        }
+
+        public double Pi
+        {
+            get
+            {
+                return Math.Round((Math.Pow(item.lamb, this.Xi) / ExtensionClass.factorial(this.Xi)) * Math.Pow(Math.E, -item.lamb),8);
+            }
+        }
+
+        public double NPi
+        {
+            get
+            {
+                return Math.Round (item.N * this.Pi,8);
+            }
+        }
+
+        public double value
+        {
+            get
+            {
+                return Math.Round (Math.Pow(this.Ni - this.NPi, 2) / this.NPi,8);
+            }
+        }
+    }
+
+    static class ExtensionClass
+    {
+        static public long factorial(int value)
+        {
+            long sum = 1;
+
+            for (int i = 1; i <= value; ++i)
+            {
+
+                sum *= i;
+            }
+
+            return sum;
+
+        }
+
+        static public List<item> generateNumbers()
+        {
+            List<item> items = new List<item>();
+
+            List<int> numbers = new List<int>();
+
+            Random rand_value = new Random();
+
+            for(int i = 0; i < 500; ++i)
+            {
+                numbers.Add(rand_value.Next(6));  
+            }
+
+            numbers.Sort();
+
+            List<int> numbers_Set = new HashSet<int>(numbers).ToList<int>();
+
+            List<int> frequency = (from i in numbers_Set select numbers.Count((j)=> { return j == i; })).ToList<int>();
+
+            for(int i = 0; i < numbers_Set.Count; ++i)
+            {
+                items.Add(new item() {Xi=numbers_Set[i],Ni=frequency[i] });
+            }
+
+            return items; ;
+        }
+
+        static public bool checkSequence(List<item> values)
+        {
+            bool iscool = false;
+
+            foreach(item i in values)
+            {
+                if(i.NPi<10 || i.Ni < 5)
+                {
+                    iscool = true;
+
+                    break;
+                }
+            }
+
+            return iscool;
+        }
+
+        static public bool checkItem(item item_Val)
+        {
+            bool iscool = false;
+
+            if(item_Val.NPi<10 || item_Val.Ni < 5)
+            {
+                iscool = true;
+            }
+
+            return iscool;
+        }
+
+        static public List<item> mergeCells(List<item> values)
+        {
+            while (checkSequence(values))
+            {       
+                for(int i = 0; i < values.Count; ++i)
+                {
+                    if (values.Count == 2)
+                    {
+                        break;
+                    }
+
+                    if (checkItem(values[i]))
+                    {
+                        if (i == values.Count - 1)
+                        {
+                            values[i - 1].Ni += values[i].Ni;
+
+                            values.Remove(values[i]);
+                        }
+                        else
+                        {
+                            values[i+1].Ni += values[i].Ni;
+
+                            values.Remove(values[i]);
+                        }
+
+                        item.lamb = values.Sum(delegate (item val) { return val.XiNi; })/item.N;
+                    }
+                }
+
+                if (values.Count == 2)
+                {
+                    break;
+                }
+
+            }
+
+            return values;
+        }
+
+        static public List<item> generateFromFile
+            (string path)
+        {
+            string[] mass=File.ReadAllLines(path);
+
+                List<int> values = (from j in mass[0].Split(' ') select int.Parse(j)).ToList();
+                List<int> frequency = (from j in mass[1].Split(' ') select int.Parse(j)).ToList();
+
+            List<item> items = new List<item>();
+
+            for(int i = 0; i < values.Count; ++i)
+            {
+                items.Add(new item() { Xi = values[i], Ni = frequency[i] });
+            }
+
+
+            return items ;
+        }
+
+    }
+    
+}
